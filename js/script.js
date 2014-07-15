9f2fd0ad53ae0c56054ce7ba93a7d57687206850
Ricky <rkthompson3@gmail.com>
2014-07-15 10:51:43 -0500

    adding camera plugin

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 function showName() {
+    var checkedValue = "";
+    var radios = document.getElementsByName("choice");
+    for (i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            checkedValue = i+1;
+        }
+    }
+    
     var nameInput = document.getElementById("nameInput");
-    alert(nameInput.value);
+    //alert(nameInput.value + " " + checkedValue);
+    
+    navigator.camera.getPicture(function(imagePath){
+        document.getElementById("photoImg").setAttribute("src", imagePath);
+    }, function(){
+        alert("Photo cancelled");
+    }, {
+        destinationType: navigator.camera.DestinationType.FILE_URI
+    });
 }

b8c940d60b45b5120dbc1022397d93f35b3b251b
Ricky <rkthompson3@gmail.com>
2014-07-15 15:58:19 -0500

    hopefully fixing camera pic src thing

--- js/script.js
+++ js/script.js
@@ -21,32 +21,7 @@ function uploadPhoto(data){
     document.getElementById('cameraPic').setAttribute('src', 'data:image/jpeg;base64,' + data);
 	//cameraPic.src = "data:image/jpeg;base64," + data;
 	// Successful upload to the server
-	navigator.notification.alert(
-		'Your Photo has been uploaded',  // message
-		okay,                           // callback
-	    'Photo Uploaded',              // title
-	    'OK'                          // buttonName
-	);
 
-	// upload has failed Fail
+    alert("got it " + data);
 
-	/* 
-
-	if (failedToUpload){
-
-	navigator.notification.alert(
-		'Your Photo has failed to upload',
-		failedDismissed,
-	    'Photo Not Uploaded',
-	    'OK'
-		);
-
-	} 
-	*/
-
-
-}
-
-function okay(){
-	// Do something
 }

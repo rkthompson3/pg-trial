c275c67f53c911b417874af96e923cbcb7907667
Ricky <rkthompson3@gmail.com>
2014-07-15 13:00:12 -0500

    adding camera plugin

--- js/script.js
+++ js/script.js
@@ -8,13 +8,44 @@ function showName() {
     }
     
     var nameInput = document.getElementById("nameInput");
-    //alert(nameInput.value + " " + checkedValue);
-    
-    navigator.camera.getPicture(function(imagePath){
-        document.getElementById("photoImg").setAttribute("src", imagePath);
-    }, function(){
-        alert("Photo cancelled");
-    }, {
-        destinationType: navigator.camera.DestinationType.FILE_URI
-    });
+    alert(nameInput.value + " " + checkedValue);
+}
+
+function capturePhoto(){
+    navigator.camera.getPicture(uploadPhoto,null,{sourceType:1,quality:60});
+}
+
+function uploadPhoto(data){
+// this is where you would send the image file to server
+
+	cameraPic.src = "data:image/jpeg;base64," + data;
+	// Successful upload to the server
+	navigator.notification.alert(
+		'Your Photo has been uploaded',  // message
+		okay,                           // callback
+	    'Photo Uploaded',              // title
+	    'OK'                          // buttonName
+	);
+
+	// upload has failed Fail
+
+	/* 
+
+	if (failedToUpload){
+
+	navigator.notification.alert(
+		'Your Photo has failed to upload',
+		failedDismissed,
+	    'Photo Not Uploaded',
+	    'OK'
+		);
+
+	} 
+	*/
+
+
+}
+
+function okay(){
+	// Do something
 }
